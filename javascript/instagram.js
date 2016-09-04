851964b639e2f572ed59ccf9d67a650032404761
PiccolaTrattoria <SergioPiccolaNY@gmail.com>
2016-09-04 13:05:31 -0400

    some changes to insatafeed

--- javascript/instagram.js
+++ javascript/instagram.js
@@ -1,6 +1,6 @@
  var userFeed = new Instafeed({
          get: 'user',
-         userId: '575314096',
+         userId: 'PiccolaTrattoria',
          clientId: '0df35003f2cb401180a91ec3d9de8c42',
  		resolution: 'thumbnail',
  		limit: '16',
