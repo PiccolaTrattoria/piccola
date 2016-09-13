4e92d5d04c2347a014673c82c00c72ea63f7e867
PiccolaTrattoria <SergioPiccolaNY@gmail.com>
2016-09-13 16:16:39 -0400

    changing menu

--- _site/javascript/instagram.js
+++ _site/javascript/instagram.js
@@ -1,7 +1,7 @@
  var userFeed = new Instafeed({
-         get: 'user',
-         userId: '3107725209',
-         clientId: '1e35a2d87bdb4edd87788bdbbdfc278b',
+        get: 'user',
+        userId: '575314096',
+        accessToken: '575314096.ba4c844.2fe22c446b204cd0b76991348fc3670e',
  		resolution: 'thumbnail',
  		limit: '16',
  		template: '<a href="{{link}}" target="_blank"><img src="{{image}}" /></a>'
